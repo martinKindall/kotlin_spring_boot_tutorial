90a98c27cbdf6890c4c04b87573e35a26ce5e0af
Martin <mart256@gmail.com>
2022-02-19 23:07:04 +0100

    First unit test of controller working.

--- src/test/kotlin/com/codigo_morsa/rest_api/controllers/LanguageControllerTest.kt
+++ src/test/kotlin/com/codigo_morsa/rest_api/controllers/LanguageControllerTest.kt
@@ -1,7 +1,5 @@
 package com.codigo_morsa.rest_api.controllers
 
-import com.codigo_morsa.rest_api.RestApiApplication
-import com.codigo_morsa.rest_api.config.WebFilters
 import com.codigo_morsa.rest_api.models.Lenguaje
 import com.codigo_morsa.rest_api.services.LenguajeService
 import org.junit.jupiter.api.BeforeAll
@@ -19,7 +17,7 @@ import org.springframework.test.web.reactive.server.WebTestClient
 import reactor.core.publisher.Mono
 import java.sql.Date
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)   // necessary for WebTestClient
 @EnableAutoConfiguration(exclude = [
     DataSourceAutoConfiguration::class,
     DataSourceTransactionManagerAutoConfiguration::class,
@@ -27,28 +25,35 @@ import java.sql.Date
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class LanguageControllerTest {
 
-//    @Autowired
-//    private lateinit var client: WebTestClient
+    @Autowired
+    private lateinit var client: WebTestClient
 
     @MockBean
     private lateinit var lenguajeService: LenguajeService
 
-    private lateinit var mockLenguaje: Lenguaje
+    private lateinit var mockLenguaje1: Lenguaje
+    private lateinit var mockLenguaje2: Lenguaje
 
     @BeforeAll
     fun setup() {
-        mockLenguaje = Lenguaje(0, "test", Date(10L), false)
+        mockLenguaje1 = Lenguaje(0, "Java", Date(10L), true)
+        mockLenguaje2 = Lenguaje(0, "Javascript", Date(10L), false)
 
-        whenever(lenguajeService.getLenguajes()).thenReturn(Mono.just(listOf(mockLenguaje)))
+        whenever(lenguajeService.getLenguajes()).thenReturn(Mono.just(listOf(mockLenguaje1, mockLenguaje2)))
     }
 
     @Test
     fun getLenguajesTest() {
-//        client.get()
-//            .uri("/lenguaje")
-//            .exchange()
-//            .expectStatus().isOk()
-//            .expectBody()
-//            .jsonPath("$.[0].nombre").isEqualTo("test")
+        client.get()
+            .uri("/lenguaje")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$.[0].nombre").isEqualTo("Java")
+            .jsonPath("$.[0].tipado_fuerte").isEqualTo("true")
+            .jsonPath("$.[0].lanzamiento").isEqualTo("1970-01-01")
+            .jsonPath("$.[1].nombre").isEqualTo("Javascript")
+            .jsonPath("$.[1].tipado_fuerte").isEqualTo("false")
+            .jsonPath("$.[1].lanzamiento").isEqualTo("1970-01-01")
     }
 }
